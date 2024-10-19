2a6f58a450105b4f2ea25fa02e66f86635ce2c0c
Carlos Valverde <cvs@odoo.com>
2024-10-19 20:16:02 +0200

    [UPG] Version 0.2

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # unblockMe
-Firefox extension to remove any class added by any website
+Firefox extension to remove any class or nodes with specific IDs added by any website
 
 **Usage**
-- Identify the classes that you want to remove.
-- Add those classes to `classesToRemove`.
+- Identify the classes and IDs that you want to remove.
+- Add those elements to `classesToRemove` and `idsToRemove`.
 - When needed, activate it by clicking on the unblockMe button.
 - Enjoy!
 
+**Application**
+- Undesirable popups.
+- Non user-friendly cookie consent forms.
+- Publicity / Ads.
+- Paywalls.
+- ...
+
 **Feel free to add or modify the extension as you wish**
+
+--
+
+**Versions**
+0.2
+
+Added - IDs remover
+
+-
+
+0.1
+
+Added - class remover
+
--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 browser.action.onClicked.addListener((tab) => {
     browser.scripting.executeScript({
         target: {tabId: tab.id},
-        func: observeAndRemoveClasses
+        func: observeAndRemoveClassesAndIDs
     });
 });
 
 function observeAndRemoveClasses() {
     const classesToRemove = ['first-class', 'second-class', 'third-class'];
+    const idsToRemove = ['first-id', 'second-id']; // List of IDs to remove
 
     // Function to remove the classes from all matching elements
     function removeClasses() {
@@ -18,12 +19,24 @@ function observeAndRemoveClasses() {
         });
     }
 
-    // Initial removal of classes
+    // Function to remove nodes containing the specified IDs
+    function removeIDs() {
+        idsToRemove.forEach(id => {
+            const element = document.getElementById(id);
+            if (element) {
+                element.remove(); // Completely remove the element from the DOM
+            }
+        });
+    }
+
+    // Initial removal of classes and nodes with IDs
     removeClasses();
+    removeIDs();
 
     // Set up a MutationObserver to monitor the DOM for changes
     const observer = new MutationObserver(() => {
         removeClasses(); // Remove classes whenever the DOM is updated
+        removeIDs();     // Remove nodes with IDs whenever the DOM is updated
     });
 
     // Start observing the document for any changes in the subtree
